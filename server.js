5214e71ebcb69dc099f7784ade4eeb33dd198757
Booker King <btking1@my.waketech.edu>
2022-07-04 09:54:20 -0400

    connect to db on server

--- server.js
+++ server.js
@@ -1,12 +1,27 @@
 const express = require("express");
+const mysql = require("mysql2");
 
 const PORT = process.env.PORT || 3001;
 const app = express();
+
+// connect to database
+const db = mysql.createConnection(
+  {
+    host: "localhost",
+    user: "Booker King",
+    password: "Adventstar789*",
+    database: "election",
+  },
+  console.log("Connected to the election database. ")
+);
 
 //middleware
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
+db.query(`SELECT * FROM candidates`, (err, rows) => {
+  console.log(rows);
+});
 
 // default response for any other request (NOT FOUND)
 app.use((req, res) => {
